53e41771737cefc52018c3cbb037248f1eb7db85
Kha Tran <khadieptran@gmail.com>
2023-03-26 01:19:23 -0400

    Implemented demo faq section on the homepage

--- frontend/src/pages/Home/HomeIndex.jsx
+++ frontend/src/pages/Home/HomeIndex.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Faq } from './components/Faq'
 import { FeaturedProduct } from './components/FeaturedProduct'
 import { Hero } from './components/Hero'
+import { Testimonials } from './components/Testimonials'
 
 export function HomeIndex() {
   return (
     <main>
       <Hero/>
       <FeaturedProduct/>
+      {/* Can have testimonial here. Not implement in this case */}
+      {/* <Testimonials/> */}
+      {/* FAQ */}
+      <Faq/>
     </main>
   )
 }
