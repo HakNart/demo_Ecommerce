a9e7c15a73275e51217a081c2122c2fe428e890c
Kha Tran <khadieptran@gmail.com>
2023-03-27 12:38:19 -0400

    Implemented Product detail page

--- frontend/src/pages/Home/HomeIndex.jsx
+++ frontend/src/pages/Home/HomeIndex.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Faq } from './components/Faq'
 import { FeaturedProduct } from './components/FeaturedProduct'
 import { Hero } from './components/Hero'
-import { Testimonials } from './components/Testimonials'
 
 export function HomeIndex() {
   return (
