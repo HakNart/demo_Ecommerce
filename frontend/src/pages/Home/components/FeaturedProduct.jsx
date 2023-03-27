6acdd389948143826c4c162f5432ff9cff439e19
Kha Tran <khadieptran@gmail.com>
2023-03-27 01:47:53 -0400

    Refactored data to product list and featured product

--- frontend/src/pages/Home/components/FeaturedProduct.jsx
+++ frontend/src/pages/Home/components/FeaturedProduct.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProductCard } from '../../../components/Elements/ProductCard'
 
 export const FeaturedProduct = () => {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("http://localhost:8001/featured_products");
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchProducts();
+  },[])
   return (
     <section className='my-20'>
       <h1 className='text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8'>
@@ -9,9 +19,10 @@ export const FeaturedProduct = () => {
       </h1>
       <div className='flex flex-wrap justify-center lg:flex-row'>
         {/* Product Card */}
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        { products.map((product) => (
+          <ProductCard key={product.id} product={product}/>
+        ))}
+
       </div>
     </section>
   )
