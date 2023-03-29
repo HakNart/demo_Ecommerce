f759c42782a1dbdf1a127d5aa77fcf4a158d9f52
Kha Tran <khadieptran@gmail.com>
2023-03-29 01:57:33 -0400

    Added dashboard view and components

--- frontend/src/pages/Dashboard/DashboardPage.jsx
+++ frontend/src/pages/Dashboard/DashboardPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+import { useTitle } from '../../hooks/useTitle';
+import { getUserOrders } from '../../services/orderServices';
+import { DashboardEmpty } from './components/DashbardEmpty';
+import { DashboardCard } from './components/DashboardCard';
 
 export function DashboardPage() {
+  const [orders, setOrders] = useState([]);
+  useTitle("Dashboard");
+
+  useEffect(()=> {
+    // Fetch all orders
+    async function fetchOrders() {
+      try {
+        const readOrders = await getUserOrders();
+        setOrders(readOrders);
+      } catch(err) {
+        toast.error(err.message);
+      }
+    }
+    fetchOrders();
+  }, [])
   return (
-    <div>
-      DashboardPage
-    </div>
+    <main>
+      <section>
+        <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">My Dashboard</p>
+      </section>
+
+      <section>
+        { orders.length && orders.map((order) => (
+          <DashboardCard key={order.id} order={order} />
+        )) }
+      </section>
+
+      <section>
+        { !orders.length && <DashboardEmpty /> }
+      </section>
+
+    </main>
   )
 }
