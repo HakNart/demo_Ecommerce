c7bb289d9f1921c9687d4e7d0a52b5a6daf8c837
Kha Tran <khadieptran@gmail.com>
2023-03-29 02:15:54 -0400

    Refactored login, register, and get user as services

---
+++ src/services/authServices.jsx
@@ -0,0 +1,67 @@
+export async function login(authDetail) {
+  const requestOptions = {
+    method: "POST",
+    headers: {"content-Type": "application/json"},
+    body: JSON.stringify(authDetail)
+  }
+  const response = await fetch(`http://localhost:8001/login`, requestOptions);
+  if(!response.ok){
+      throw { message: response.statusText, status: response.status }; 
+  }
+  const data = await response.json();
+
+  if(data.accessToken){
+      sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+      sessionStorage.setItem("uid", JSON.stringify(data.user.id));
+  }
+
+  return data;
+}
+
+export async function register(authDetail){
+  const requestOptions = {
+      method: "POST",
+      headers: {"content-Type": "application/json"},
+      body: JSON.stringify(authDetail)
+  }  
+  const response = await fetch(`http://localhost:8001/register`, requestOptions);
+  if(!response.ok){
+      throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
+  const data = await response.json();
+  
+  if(data.accessToken){
+      sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+      sessionStorage.setItem("uid", JSON.stringify(data.user.id));
+  }
+
+  return data;
+}
+
+export function logout(){
+  sessionStorage.removeItem("token");
+  sessionStorage.removeItem("uid");
+}
+
+function getSession() {
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const uid = JSON.parse(sessionStorage.getItem("uid"));
+  return { token, uid };
+}
+
+export const getUser = async () => {
+  const browserData = getSession();
+  const requestOptions = {
+    method: "GET",
+    headers: { 
+      "Content-Type": "application/json", 
+      Authorization: `Bearer ${browserData.token}` 
+    }
+  }
+  const response = await fetch(`http://localhost:8001/600/users/${browserData.uid}`, requestOptions);
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; 
+  }
+  const data = await response.json();
+  return data;
+}
