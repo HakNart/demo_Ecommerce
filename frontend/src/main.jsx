cdea68b1a857f10fc7fdc3d06772614f778eed5b
Kha Tran <khadieptran@gmail.com>
2023-03-28 21:06:29 -0400

    Implemented basic register function and provide toast message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App'
 import { ScrollToTop } from './components/Other/ScrollToTop'
 import { FilterProvider } from './context/FilterContext'
@@ -11,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <FilterProvider>
         <ScrollToTop/>
+        <ToastContainer closeButton={false} position={'bottom-left'} autoClose={2000}/>
         <App />       
       </FilterProvider>
     </BrowserRouter>
