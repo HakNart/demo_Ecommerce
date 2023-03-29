5c571a1359b3697bf57cde78fc1da6ab11ea3cda
Kha Tran <khadieptran@gmail.com>
2023-03-29 00:14:31 -0400

    Added Cart features

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useCart } from '../../context/CartContext';
 import { useTitle } from '../../hooks/useTitle';
 import { CartEmpty } from './components/CartEmpty';
 import { CartList } from './components/CartList';
 
 export function CartPage() {
-  const cartList = [1];
+  const {cartList} = useCart();
   useTitle(`Cart (${cartList.length})`);
 
   return (
