d114845d1e8de4730b56c870854353995995f168
Kha Tran <khadieptran@gmail.com>
2023-03-28 22:33:23 -0400

    Set up view and components for cart page

--- frontend/src/pages/Cart/CartPage.jsx
+++ frontend/src/pages/Cart/CartPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useTitle } from '../../hooks/useTitle';
+import { CartEmpty } from './components/CartEmpty';
+import { CartList } from './components/CartList';
 
 export function CartPage() {
+  const cartList = [1];
+  useTitle(`Cart (${cartList.length})`);
+
   return (
-    <div>
-      Cart page
-    </div>
+    <main>       
+      { cartList.length ? <CartList /> : <CartEmpty /> }   
+    </main>
   )
 }
