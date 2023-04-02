fae92b8aff500232e3bae3a164c4b3ac2a7c2562
Kha Tran <khadieptran@gmail.com>
2023-04-02 18:43:57 -0400

    Refactored react app to connect to spring api backend application

--- src/services/orderServices.jsx
+++ src/services/orderServices.jsx
@@ -12,7 +12,8 @@ export async function getUserOrders(){
       method: "GET",
       headers: {"Content-Type": "application/json", Authorization: `Bearer ${browserData.token}`}
   }
-  const response = await fetch(`${host}/users/660/orders?user.id=${browserData.uid}`, requestOptions);
+  // const response = await fetch(`${host}/users/660/orders?user.id=${browserData.uid}`, requestOptions);
+  const response = await fetch(`${host}/users/orders?user.id=${browserData.uid}`, requestOptions);
   if(!response.ok){
       throw { message: response.statusText, status: response.status }; //eslint-disable-line
   }
@@ -26,18 +27,20 @@ export async function createOrder(cartList, total, user){
       cartList: cartList,
       amount_paid: total,
       quantity: cartList.length,
-      user: {
-          name: user.name,
-          email: user.email,
-          id: user.id
-      }
+      // user: {
+      //     name: user.name,
+      //     email: user.email,
+      //     id: user.id
+      // }
+      userId: user.id,
   }
   const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json", Authorization: `Bearer ${browserData.token}` },
       body: JSON.stringify(order)
   }
-  const response = await fetch(`${host}/660/orders`, requestOptions);
+  // const response = await fetch(`${host}/660/orders`, requestOptions);
+  const response = await fetch(`${host}/orders`, requestOptions);
   if(!response.ok){
       throw { message: response.statusText, status: response.status }; //eslint-disable-line
   }
